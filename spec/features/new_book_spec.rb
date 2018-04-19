0be299410d58b89d649e8958758e408ba16e8d99
Hannah Lee and Russell Hofvendahl <hannah@>
2018-04-19 09:36:05 -0700

    write passing integration test for edit.

--- spec/features/new_book_spec.rb
+++ spec/features/new_book_spec.rb
@@ -9,4 +9,15 @@ describe "add a new book process" do
     click_on 'Create Book'
     expect(page).to have_content 'Learn How To Program'
   end
+
+  it "allows edit of a book" do
+    new_book = Book.create(title: 'Learn How To Program',
+                            published: 2012,
+                            author: 'Michael Nyman')
+    visit edit_book_path(new_book)
+    fill_in 'Title', :with => 'Learn How To Code'
+    fill_in 'Author', :with => 'Michael Nyman'
+    click_on 'Update Book'
+    expect(page).to have_content 'Learn How To Code'
+  end
 end
